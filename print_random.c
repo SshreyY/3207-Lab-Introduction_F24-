9b2a046770a0b6fc8b3d252fab526679aeff0134
SshreyY <tup85783@temple.edu>
2024-08-29 13:07:24 -0400

    Changes to be committed: 	modified:   print_random.c

--- print_random.c
+++ print_random.c
@@ -1,10 +1,32 @@
-// Given Project code
-char *rand_string_alloc(size_t size)
+#include <stdio.h>
+#include <stdlib.h>
+#include "random.c"
+size_t size = 7;
+// char *rand_string_alloc(char z, size_t size);
+
+int main(void)
 {
-    char *s = malloc(size + 1);
-    if (s)
+    // rand_string_alloc(7);
+    srand(time(NULL));
+    char s[size];
+    int count = 0;
+    while (count != 7)
     {
-        rand_string(s, size);
+        s[count] = randchar();
+        count++;
     }
-    return s;
+    printf("random: %s\n", s);
+    // *rand_string_alloc(s, size);
 }
+
+// Given Project code
+// char *rand_string_alloc(size_t size)
+// {
+
+//     char *s = malloc(size + 1);
+//     if (s)
+//     {
+//         rand_string(s, size);
+//     }
+//     return s;
+// }
