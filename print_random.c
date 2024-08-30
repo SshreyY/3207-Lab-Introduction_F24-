aaba364a2113088b9632cfbd61d77a90c59f79e5
SshreyY <tup85783@temple.edu>
2024-08-29 22:16:09 -0400

    Changes to be committed: 	modified:   print_random.c 	modified:   random.c

--- print_random.c
+++ print_random.c
@@ -2,23 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include "random.h"
-
+// intialized the size variable here so that it is accessible all throughout the file
 size_t size = 7;
+
+// function prototypes
 char rand_string(char *s, size_t size);
 char *rand_string_alloc(size_t size);
 
 int main(void)
 {
     srand(time(NULL));
-
+    // creating a new char variable to store the string that is produced in rand_string_alloc
     char *s = rand_string_alloc(size);
     if (s)
     {
         printf("Random String of 7 characters: %s\n", s);
-        free(s);
+        free(s); // freeing the string stored on RAM
     }
 }
 
+/*this function below takes in an empty s pointer char that is memory allocated
+by the given size in the main to store the random seven characters that are
+produced randomly by randchar() in random.c file
+*/
 char rand_string(char *s, size_t size)
 {
     for (int i = 0; i < size; i++)
--- random.c
+++ random.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
+// initializing  the max variable, so that it is available throught the file
 const int max = 26;
 
+// the function below returns a random letter from the alphabets
 char randchar()
 {
     char alphabets[] = "ABCDEFGHIJKLMNOPQRSTUVXWYZ";
