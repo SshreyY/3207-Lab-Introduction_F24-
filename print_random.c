21a0c3987dedd92e96906b8180d2047af8f8b812
SshreyY <tup85783@temple.edu>
2024-08-30 12:30:21 -0400

    Changes to be committed: 	modified:   print_random.c

--- print_random.c
+++ print_random.c
@@ -1,47 +1,21 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "random.h"
-// intialized the size variable here so that it is accessible all throughout the file
-size_t size = 7;
 
-// function prototypes
-char rand_string(char *s, size_t size);
-char *rand_string_alloc(size_t size);
-
-int main(void)
+int main()
 {
-    srand(time(NULL));
-    // creating a new char variable to store the string that is produced in rand_string_alloc
-    char *s = rand_string_alloc(size);
-    if (s)
-    {
-        printf("Random String of 7 characters: %s\n", s);
-        free(s); // freeing the string stored on RAM
-    }
-}
+    int a;
 
-/*this function below takes in an empty s pointer char that is memory allocated
-by the given size in the main to store the random seven characters that are
-produced randomly by randchar() in random.c file
-*/
-char rand_string(char *s, size_t size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        s[i] = randchar();
-    }
-    return *s;
-}
+    /* seed the randomizer */
+    srand((unsigned)time(NULL));
 
-// Given Project code
-char *rand_string_alloc(size_t size)
-{
+    printf("Today's random word: ");
+    // the for loop below will loop 7 times and get a random character from random.c file
+    for (a = 0; a < 7; a++)
+        putchar(randchar());
+    putchar('\n');
 
-    char *s = malloc(size + 1);
-    if (s)
-    {
-        rand_string(s, size);
-    }
-    return s;
+    return (0);
 }
